14d2ea12fdfd2420b8b9448414b8dd897746b4a1
sanjit <sanjeet291196@gmail.com>
2016-12-18 01:02:56 +0530

    Authentications

--- app/src/main/java/hitam/epics/sahaya/RegistrationActivity.java
+++ app/src/main/java/hitam/epics/sahaya/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package hitam.epics.sahaya;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+
+    public void register(View view) {
+        TextInputLayout email = (TextInputLayout) findViewById(R.id.registration_email);
+        TextInputLayout password = (TextInputLayout) findViewById(R.id.registration_password);
+
+        new Sahaya().register(email.getEditText().getText().toString(), password.getEditText().getText().toString());
     }
 }
--- app/src/main/java/hitam/epics/sahaya/Sahaya.java
+++ app/src/main/java/hitam/epics/sahaya/Sahaya.java
@@ -2,21 +2,92 @@ package hitam.epics.sahaya;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Sahaya extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sahaya);
-        new Handler().postDelayed(new Runnable() {
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(Sahaya.this, HomeActivity.class));
-                Sahaya.this.finish();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    startActivity(new Intent(Sahaya.this, DashboardActivity.class));
+                } else {
+                    startActivity(new Intent(Sahaya.this, HomeActivity.class));
+                }
             }
-        },1500);
+        };
+    }
+
+    public void register(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("FIREBASE", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Sahaya.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("FIREBASE", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("FIREBASE", "signInWithEmail", task.getException());
+                            Toast.makeText(Sahaya.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
