491b28c7a2511dd6a798d93d3eec097d04af321f
sanjit <sanjeet291196@gmail.com>
2016-12-28 20:57:43 +0530

    Material Design

--- app/src/main/java/hitam/epics/sahaya/Sahaya.java
+++ app/src/main/java/hitam/epics/sahaya/Sahaya.java
@@ -1,11 +1,12 @@
 package hitam.epics.sahaya;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class Sahaya extends AppCompatActivity {
+public class Sahaya extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
